47f4904561058382a66511d6b345238b46812034
KERDANET Loic <loic943@gmail.com>
2022-09-11 22:10:59 +0200

    ajout Tailwing CSS en cdn

--- public/admin/login.php
+++ public/admin/login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once(__DIR__ . '/../../fonctions.php');
 
 if ($_SESSION['admin'] ?? null) {
@@ -14,7 +14,10 @@ if (is_post()) {
         $_SESSION['admin'] = $admin;
         redirect('/admin/dashboard.php');
     } else {
-        $errors['credentials'] = 'Identifiants incorrects';
+        $_SESSION['previous_errors']['credentials'] = 'Identifiants incorrects';
+        $_SESSION['previous_inputs']['name'] = $_POST['name'];
+        $_SESSION['previous_inputs']['password'] = $_POST['password'];
+        redirect('/admin/login.php');
     }
 }
 
@@ -22,25 +25,31 @@ if (is_post()) {
 
 <?php affiche('header', ['title' => 'Connexion Admin']) ?>
 
-<h1>Connexion Admin</h1>
-
-<form method="post">
-    <?php if (isset($errors['credentials'])): ?>
-    <p>
-        <?= $errors['credentials'] ?>
-    </p>
-    <?php endif ?>
-    <p>
-        <label for="name">Nom</label>
-        <input type="text" name="name" id="name">
-    </p>
-    <p>
-        <label for="password">Mot de Passe</label>
-        <input type="password" name="password" id="password">
-    </p>
-    <p>
-        <button>Connexion</button>
-    </p>
-</form>
+<div class="min-w-screen min-h-screen flex justify-center items-start bg-gray-200">
+    
+    <div class="bg-white shadow-lg p-8 mt-8">
+        
+        <h1 class="text-xl mb-4">Connexion Admin</h1>
+    
+        <form method="post">
+            <?php if (isset($previous_errors['credentials'])) : ?>
+                <p class="w-full border border-red-900 bg-red-200 text-red-900 mb-4 px-3 py-1">
+                    <?= $previous_errors['credentials'] ?>
+                </p>
+            <?php endif ?>
+            <p class="mb-3">
+                <label for="name" class="block text-sm px-3">Nom</label>
+                <input type="text" name="name" id="name" class="border focus:border-black px-3 py-1 w-full" value="<?= $previous_inputs['name'] ?? '' ?>" required>
+            </p>
+            <p class="mb-3">
+                <label for="password" class="block text-sm px-3">Mot de Passe</label>
+                <input type="password" name="password" id="password" class="border focus:border-black px-3 py-1 w-full" required>
+            </p>
+            <p class="mt-6">
+                <button class="border w-full py-1 shadow-lg bg-gray-100">Connexion</button>
+            </p>
+        </form>
+    </div>
+</div>
 
 <?php affiche('footer') ?>
